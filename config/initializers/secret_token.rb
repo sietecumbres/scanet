388fef0a99a9b00bf72d786fb236cd62938de3d8
Juan Espinosa <juan@hlmbusiness.com>
2011-03-14 23:43:05 -0500

    Project skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scanet::Application.config.secret_token = '62c0a7d20046ae6561c0d6e3bffab659d81dd4a0e528fd6dc981a1f602d4c7c07c2772e836aa89eda0d087ea8a18889b20da041d149d168847d053c93cccef20'
